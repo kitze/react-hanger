ed0e652226c13d2892c06b7787d059761dd9e524
Andrey Los <puha212@gmail.com>
2020-04-19 14:41:15 +0200

    build: setup of np tool, for to ease publishing process

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,7 +1,7 @@
-export * from './useArray';
-export * from './useBindToInput';
-export * from './useBoolean';
-export * from './useInput';
-export * from './useMap';
-export * from './useNumber';
-export * from './useSetState';
+export { useBindToInput, UseBindToInput, BindToInput } from './useBindToInput';
+export { useArray, UseArray, UseArrayActions } from './useArray';
+export { useBoolean, UseBoolean, UseBooleanActions } from './useBoolean';
+export { useInput, UseInput, UseInputActions } from './useInput';
+export { useMap, UseMap, MapOrEntries, UseMapActions, UseMapFunctions } from './useMap';
+export { useNumber, UseNumber, UseNumberActions } from './useNumber';
+export { useSetState, UseSetState, UseSetStateAction } from './useSetState';
