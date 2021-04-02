041e54ff594ec45e80903b1eaf2c960fe5bde6d1
Praneet Rohida <praneetrohida@gmail.com>
2021-04-02 11:32:20 +0530

    remove redundant hooks

--- src/useSizzyHooks.ts
+++ src/useSizzyHooks.ts
@@ -55,34 +55,6 @@ export const useVisiblePose = (initial: any) => {
   return [pose, (v: any) => setPose(v ? VISIBLE : HIDDEN), rest];
 };
 
-function gtag(...args: any[]) {
-  // @ts-ignore
-  window.dataLayer.push(args);
-}
-
-export const useGoogleAnalytics = (id: string, startLoading: boolean) => {
-  useEffect(() => {
-    if (!id) {
-      return;
-    }
-
-    if (startLoading) {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
-      document.body.appendChild(script);
-      // @ts-ignore
-      window.dataLayer = window.dataLayer || [];
-
-      gtag('js', new Date());
-      gtag('config', id, {
-        anonymize_ip: true,
-        cookie_expires: 0,
-      });
-    }
-  }, [id, startLoading]);
-};
-
 export const useFindElementCenter = (elementRef: RefObject<HTMLElement>) => {
   const [windowSize, setWindowSize] = useState<{ x: number; y: number }>();
   useEffect(() => {
@@ -133,45 +105,4 @@ export const useClock = (
   }, [getCurrentTime]);
 
   return time;
-};
-
-export const useOnPageLoad = () => {
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    window.onload = () => setLoaded(true);
-  }, []);
-  return loaded;
-};
-
-type UseScript = { startLoading: boolean; src: string };
-
-export const useLoadScript = ({ startLoading, src }: UseScript) => {
-  const [state, setState] = useState<{ ready: boolean; error: string | null | Event }>({
-    ready: false,
-    error: null,
-  });
-
-  useEffect(() => {
-    if (startLoading) {
-      const script = document.createElement('script');
-      script.src = src;
-      script.onload = () => setState({ ready: true, error: null });
-      script.onerror = (error) => setState({ ready: false, error });
-      document.body.appendChild(script);
-    }
-  }, [src, startLoading]);
-
-  return state;
-};
-
-export const useScript = (props: UseScript) => useLoadScript(props);
-
-export const useDelay = (ms: number) => {
-  const [done, setDone] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setDone(true);
-    }, ms);
-  }, [ms]);
-  return done;
 };
